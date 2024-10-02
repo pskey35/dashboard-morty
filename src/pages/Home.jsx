e8d692bfd8fa21c0b361810315be1d18544978ea
pskey35 <anonimo35371@gmail.com>
2024-10-01 19:15:49 -0500

    ahora esta mejor maquetado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,42 @@ function Home() {
             <div className="container_left w-[10%] max-w-[100px]">
                 <Menu></Menu>
             </div>
-            <div className="container_right">
+            <div className="container_right flex-1">
+                <div className='top-0 left-0 w-full flex justify-around items-center border border-gray-500 h-14 bg-white'>
+                    <h2>MODULO DE GESTION DE MEMORIA DESCRIPTIVA LST 4</h2>
+                    <div>
+                        <label htmlFor="">Ingrese la foto JEAN MARTIN</label>
+                        <select name="" id="">
+                            <option value="">Configuracion</option>
+                            <option value="">Salir</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="flex-1 mt-16 ml-16 md:ml-48 p-5">
 
+
+                    <div className='mt-16 h-full'>
+                        <p>Listado de precios</p>
+                        <div className='flex h-full justify-around items-center'>
+                            <p>Filtros de Búsqueda de Predios</p>
+                            <div className='flex'>
+                                <div>
+                                    <label>Proyecto</label>
+                                    <select name="" id="">
+                                        <option value="">Proyecto 1</option>
+                                        <option value="">Proyecto 2</option>
+                                        <option value="">Proyecto 3</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label htmlFor="">Código de Precio</label>
+                                    <input type="text" placeholder='Codigo de precio' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
